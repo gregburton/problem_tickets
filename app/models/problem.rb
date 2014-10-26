c9f0f84e919e2f6506661373106f1c0f4f8fa4a8
Nick Passarella <nickpassarella@gmail.com>
2014-10-26 10:48:20 -0400

    Added timestamp to listing on problem index. Problems list in ascending order. Description truncated

--- app/models/problem.rb
+++ app/models/problem.rb
@@ -1,6 +1,7 @@
 class Problem < ActiveRecord::Base
   belongs_to :user
   has_many :notes
+  default_scope -> { order('created_at ASC') }
 
   validates :description, presence: true
   validates :attempt, presence: true
--- db/seeds.rb
+++ db/seeds.rb
@@ -16,11 +16,12 @@ end
 15.times do
   Problem.create(description: "#{["How would I", "Can someone tell me how to"].sample} #{Faker::Company.bs}?",
                  attempt: "I've tried to #{Faker::Company.bs} and #{Faker::Company.bs}, but #{["I'm stuck", "I can't figure it out"].sample}!",
-                 user_id: rand(1..50))
+                 user_id: rand(1..50),
+                 created_at: rand(1.year).ago)
 end
 
 35.times do
-  Note.create(text: "#{["Have you tried", "What if you"].sample} #{Faker::Company.bs}? Then you can #{Faker::Company.bs}!",
+  Note.create(text: "#{["Have you tried to", "What if you"].sample} #{Faker::Company.bs}? Then you can #{Faker::Company.bs}!",
               user_id: rand(1..50),
               problem_id: rand(1..15))
 end
