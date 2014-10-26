3ebf78217e99f3d60a98e9bbb6cd389c74c5efdf
gregurton <gburt802@gmail.com>
2014-10-25 20:34:11 -0400

    registration, login, and logout are now all working

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,9 @@ Rails.application.routes.draw do
     resources :notes, :only => [:create]
   end
 
-  delete 'login/destroy'
+  delete 'logout' => 'logins#destroy'
+
+  #delete 'login/destroy'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
