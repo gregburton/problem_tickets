721a4a67e1e7269b4326cad59de106eb8b088152
gregurton <gburt802@gmail.com>
2014-11-11 08:17:46 -0500

    made additional cleanup changes

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -5,11 +5,13 @@ ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
 
-class ActiveSupport::TestCase
-  # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
-  fixtures :all
+class ActiveSupport
+  class TestCase
+    # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
+    fixtures :all
 
-  def logged_in_session
-    { current_user_id: users(:one).id }
+    def logged_in_session
+      { current_user_id: users(:one).id }
+    end
   end
 end
